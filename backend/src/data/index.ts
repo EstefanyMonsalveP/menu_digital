dd929f2e5aaeb36cbf3f6a8f94ae5c80ba37b643
Estefany Monsalve <monsalvepinoe@gmail.com>
2025-08-16 17:08:21 -0500

    feat(index.ts): Crear el index.ts como punto de entrada del backend

---
+++ backend/src/data/index.ts
@@ -0,0 +1,17 @@
+import Express from "express";
+import { conexionDB } from "./db.js";
+
+const app = Express();
+//Obtiene el puerto desde la variable de entorno
+const PORT = process.env.PORT;
+
+//Si no esta definido el puerto, enviar el error y detener la ejecución.
+if(!PORT)throw new Error("Se presentaron problemas para inicializar el puerto");
+
+conexionDB(); //Se invoca la función para conectar la base de datos.
+
+//Inicializa el servidor en el puerto definido
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
+
