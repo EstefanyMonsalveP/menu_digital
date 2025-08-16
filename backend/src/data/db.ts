2e41d0352454ac3027f3de8da7ad5f45596084f2
Estefany Monsalve <monsalvepinoe@gmail.com>
2025-08-16 16:57:05 -0500

    feat(db.ts): Crea la conexion con la base de datos

---
+++ backend/src/data/db.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+//Variable de entorno para la conexión de la DB
+const MONGO_URI = process.env.MONGO_URI;
+
+//Si no existe la variable de conexión, lanzar el error.
+if(!MONGO_URI) throw new Error("La variable de entorno MONGO_URI no esta definida");
+
+//Conexion con la base de datos
+export const conexionDB = async (): Promise <void> => {
+    try{
+        await mongoose.connect(MONGO_URI);
+        console.log("Conexion exitosa");
+    }catch (error){
+        console.log("Error en la conexion");
+        process.exit(1); //Cerrar el proceso si se presenta error.
+    }
+}
