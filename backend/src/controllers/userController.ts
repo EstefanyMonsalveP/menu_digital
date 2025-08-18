92271c1189d4e86c31468982697ed384e836a8df
Estefany Monsalve <monsalvepinoe@gmail.com>
2025-08-17 23:30:43 -0500

    feat(userService): Crear funcion authenticateUser para validar las credenciales del usuario

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -2,27 +2,37 @@ import { Request, Response } from "express";
 import { User} from "../models/user";
 import { checkUserExists } from "../services/userService";
 import { userRegisterSchema } from "../schema/user.schema";
+import { ZodError } from "zod";
 
 //Función para crear un nuevo usuario
 export const createUser = async (req: Request, res: Response) => {
     try {
+        //Se invoca la función para hacer las validaciones de los campos.
+        const validatedData = userRegisterSchema.parse(req.body);
 
-            const validatedData = userRegisterSchema.parse(req.body);
-        //Invoca la función para validar los datos del usuario
+        //Invoca la función para validar el username y email.
         await checkUserExists(validatedData.username, validatedData.email);
 
         //Despues de la validación, continua con la creación del usuario
-        const newUser = new User({
-            validatedData
-        })
+        const newUser = new User(validatedData)
 
-        //Guarda el nuevo plato en la base de datos.
+        //Guarda el nuevo usuario en la base de datos.
         await newUser.save();
 
-        return res.status(201).json({message: "Usuario creado con exito", data: newUser})
+        return res.status(201).json({message: "Usuario creado con exito"})
     } catch (error) {
-        //Envia respuesta de error con el mensaje
+        //Envia los mensajes de error si provienen de Zod
+        if (error instanceof ZodError) {
+            // Devolver solo los mensajes de error
+            const errores = error.issues.map((err) => err.message);
+            return res.status(400).json({
+            message: "Error en los datos",
+            errors: errores,
+      });
+    }
+        //Envia el error con el mensaje si proviene del servidor
         return res.status(500).json({message: "Error al crear al usuario"})
     }
 }
+
 
--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,4 +1,5 @@
 import { User} from "../models/user";
+import bcrypt from "bcrypt";
 
 //Función para validar si existe el username y email
 export const checkUserExists = async (username:string, email:string) => {
@@ -13,4 +14,19 @@ export const checkUserExists = async (username:string, email:string) => {
     //Si encuentra el mismo correo devuelve el error
     if(existingEmail)
     throw new Error("El email ya se encuentra registrado");
+}
+
+//Validar si el usuario existe en la base de datos
+export const authenticateUser = async (username: string , password:string ) => {
+    const user = await User.findOne({username});
+
+    //Si el usuario no existe envia el error
+    if(!user) throw new Error("El usuario no existe"); 
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if(!isValidPassword) throw new Error("Contraseña incorrecta");
+
+    return user;
+    
 }
