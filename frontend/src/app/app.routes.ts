a86c61137bfd2b48250596ab78b54e05b7ec3a86
Estefany Monsalve <monsalvepinoe@gmail.com>
2025-08-19 12:48:52 -0500

    feat(auth.service): Implementar login y logout con cookies HttpOnly

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CrudComponent } from './crud/crud';
+import { Login } from './login/login';
 
 export const routes: Routes = [
-    {path: 'crud', component: CrudComponent}
+    {path: 'crud', component: CrudComponent},
+    {path: 'login', component: Login}
 ];
---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable} from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+interface LoginResponse{
+    token:string;
+    userName:string
+}
+
+@Injectable({providedIn: 'root'})
+export class authService{
+    private apiUrl = 'http://localhost:3000/api/auth';
+
+    constructor(private http: HttpClient){}
+    //Metodo para inciar la sesion
+    //Llama el servicio para validar las credenciales del usuario
+    login(email: string, password:string): Observable<{userName: string}>{
+        return this.http.post<{userName: string}>(
+            `${this.apiUrl}/login`,
+            { email, password },
+            { withCredentials: true }
+        );
+    }
+    //Metodo para cerrar sesion
+    //Llama el servicio para eliminar la cookie de sesion
+    logout() {
+    return this.http.post(`${this.apiUrl}/logout`, {}, { withCredentials: true });
+  }
+}
