9a15d25f8f8c4fbe8cd3b1da86678e936b3dd32b
Estefany Monsalve <monsalvepinoe@gmail.com>
2025-08-19 20:29:49 -0500

    feat: añadir componente para registrar los usuarios

--- frontend/src/app/registro/registro.ts
+++ frontend/src/app/registro/registro.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControl } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './registro.html',
   styleUrl: './registro.css'
 })
 export class Registro {
 
+  name = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  register() {
+    //Valida que las contraseñas coincidan
+    if (this.password !== this.confirmPassword) {
+      alert('Las contraseñas no coinciden');
+      return;
+    }
+
+    //Crea el nuevo usuario
+    this.userService.createUser({
+    name: this.name,
+    email: this.email,
+    password: this.password
+  }).subscribe({
+    next: () => {
+      alert('Usuario creado correctamente. Ahora inicia sesión.');
+      this.name = '';
+      this.email = '';
+      this.password = '';
+      this.confirmPassword = '';
+    },
+    error: err => console.error(err)
+  });
+}
 }
