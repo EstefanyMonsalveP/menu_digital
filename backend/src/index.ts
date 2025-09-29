49be872cd58391fb6419105ea095d160b87308f2
Estefany Monsalve <monsalvepinoe@gmail.com>
2025-09-29 09:38:29 -0500

    feat(index): usar .env.production en producción y .env en desarrollo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({
+  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env"});
 
 import Express from "express";
 import { conexionDB } from "./data/db";
