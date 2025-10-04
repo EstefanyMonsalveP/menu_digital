20530879fcb8104c306524abf23146ed8f1dfd67
Estefany Monsalve <monsalvepinoe@gmail.com>
2025-10-04 16:55:01 -0500

    feat(gitignore): Agregar environmets

--- frontend/src/app/services/dish.service.ts
+++ frontend/src/app/services/dish.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient , HttpHeaders } from "@angular/common/http";
 import { Dish } from "../models/dish";
 import { Observable,map } from "rxjs";
-import { environment } from "../../environment";
+import { environment } from "../../environments/environment";
 
 @Injectable({
     providedIn: 'root'
