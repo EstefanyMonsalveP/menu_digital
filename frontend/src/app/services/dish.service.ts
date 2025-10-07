00d9723a6bc9af47e4d6c21dc20f94ece38cdac0
Estefany Monsalve <monsalvepinoe@gmail.com>
2025-10-07 15:25:50 -0500

    fix(dish.service): Organizar las rutas del backend

--- frontend/src/app/services/dish.service.ts
+++ frontend/src/app/services/dish.service.ts
@@ -14,22 +14,22 @@ export class DishService {
 
     //Obtener los platos
      getDishes(): Observable<Dish[]> {
-    return this.http.get<{ data: Dish[] }>(this.apiUrl, { withCredentials: true })
+    return this.http.get<{ data: Dish[] }>(`${environment.apiUrl}/dishes`, { withCredentials: true })
       .pipe(map(res => res.data)); // extrae directamente el array de platos
   }
 
     //Agregar un plato
     addDish(dish: Dish): Observable<Dish[]>{
-        return this.http.post<Dish[]>(this.apiUrl,dish, {withCredentials:true})
+        return this.http.post<Dish[]>(`${environment.apiUrl}/dishes`,dish, {withCredentials:true})
     }
 
     //Actualizar un plato
     updateDish(id: string, dish: Dish): Observable<Dish[]>{
-        return this.http.put<Dish[]>(`${this.apiUrl}/${id}`,dish, {withCredentials:true})
+        return this.http.put<Dish[]>(`${environment.apiUrl}/dishes/${id}`,dish, {withCredentials:true})
     }
 
     //Eliminar un plato
     deleteDish(id: string): Observable<Dish[]>{
-        return this.http.delete<Dish[]>(`${this.apiUrl}/${id}`, {withCredentials:true})
+        return this.http.delete<Dish[]>(`${environment.apiUrl}/dishes/${id}`, {withCredentials:true})
     }
 }
